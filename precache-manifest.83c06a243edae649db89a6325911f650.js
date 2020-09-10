fd2b16851cb7d3283e8c1cd6e341fe81c7081bde
Nedra Krifa <nedrakrifa@gmail.com>
2020-09-10 13:30:48 +0200

    Updates

---
+++ precache-manifest.83c06a243edae649db89a6325911f650.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d02e221fdbf25daf256b730bf62ac22a",
+    "url": "/react-weather-app/index.html"
+  },
+  {
+    "revision": "d966e6ef79d39b3c3185",
+    "url": "/react-weather-app/static/css/main.c7e27b11.chunk.css"
+  },
+  {
+    "revision": "311d1a8cbebe2f449784",
+    "url": "/react-weather-app/static/js/2.f1b3e518.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-weather-app/static/js/2.f1b3e518.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d966e6ef79d39b3c3185",
+    "url": "/react-weather-app/static/js/main.2becc006.chunk.js"
+  },
+  {
+    "revision": "5e86c695d3836a3aa3c7",
+    "url": "/react-weather-app/static/js/runtime-main.5416ceab.js"
+  }
+]);
